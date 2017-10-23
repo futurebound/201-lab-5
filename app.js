7a2f6fa9652365a94df46cbe691643b475fcc6d7
Mitchell M <mitchellm1618@gmail.com>
2017-10-22 17:05:48 -0700

    problem 3 testing positive

--- app.js
+++ app.js
@@ -30,14 +30,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// TODO: Write your code here
+// DONE: Write your code here
 function multiply(a,b){ //eslint-disable-line
   var theProduct = a * b;
   var message = 'The product of ' + a + ' and ' + b + ' is ' + theProduct + '.';
   return [theProduct, message];
 }
 
-// TODO: Here is the test for multiply(); uncomment it to run it
+// DONE: Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -53,13 +53,17 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// TODO: Write your code here
+// DONE: Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var theSum = sum(a,(sum(b,c)));
+  var theProduct = multiply((sum(a,b)),c);
+  var theSumMessage = a + ' and ' + b + ' and ' + c + ' sum to 16.';
+  var theProductMessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is 140.';
+  return [theSum, theProduct, theSumMessage, theProductMessage];
 }
 
-// TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+// DONE: Here is the test for sumAndMultiply(); uncomment it to run it
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
